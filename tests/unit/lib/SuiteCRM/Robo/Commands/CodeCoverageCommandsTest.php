e23b2d9172d7b96e8dfda95c6547a02a8ee401c8
Daniel Samson <daniel.samson@salesagility.com>
2018-05-09 14:36:07 +0100

    correcting extension typo

--- tests/unit/lib/SuiteCRM/Robo/Commands/CodeCoverageCommandsTest.php
+++ tests/unit/lib/SuiteCRM/Robo/Commands/CodeCoverageCommandsTest.php
@@ -1,4 +1,7 @@
 <?php
+
+use \SuiteCRM\Robo\Plugin\Commands\CodeCoverageCommands;
+
 class CodeCoverageCommandsTest extends \Codeception\Test\Unit
 {
     /**
@@ -6,16 +9,57 @@ class CodeCoverageCommandsTest extends \Codeception\Test\Unit
      */
     protected $tester;
 
+    /** @var \SuiteCRM\Robo\Plugin\Commands\CodeCoverageCommands **/
+    protected static $testClass;
+
     public function _before()
     {
         parent::_before();
+
+        if (self::$testClass === null) {
+            self::$testClass = new CodeCoverageCommands();
+        }
     }
+
+    protected function testIsEnvironmentTravisCI()
+    {}
+
+    protected function testGetCommitRangeForTravisCi()
+    {}
+
+    protected function testIsTravisPullRequest()
+    {}
+
+    protected function testGitFilesChanged()
+    {}
+
+    protected function testFilterFilesByExtension()
+    {}
+
+    protected function testConfigureCodeCoverageFiles()
+    {}
+
+    protected function testGenerateEmptyCodeCoverageFile()
+    {}
+
+    protected function testGenerateCodeCoverageFile()
+    {}
+
+    protected function testDisableStateChecker()
+    {}
 
     public function testGetCodeceptionYml()
     {
-        $reflection = new ReflectionClass(\SuiteCRM\Robo\Plugin\Commands\CodeCoverageCommands::class);
+        $reflection = new ReflectionClass(CodeCoverageCommands::class);
         $method = $reflection->getMethod('getCodeceptionYml');
         $method->setAccessible(true);
-        $this->assertNotEmpty($method->invoke(new \SuiteCRM\Robo\Plugin\Commands\CodeCoverageCommands()));
+        $this->assertNotEmpty($method->invoke(self::$testClass));
     }
+
+    protected function testSetCodeceptionYml()
+    {}
+
+    protected function testGetCodeceptionYmlPath()
+    {}
+
 }
